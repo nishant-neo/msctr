225066553569322baf9b2242e948a26975ae4b13
sups <sups@localhost.localdomain>
2017-08-05 18:27:41 +0530

    os-independent

--- tissueSegmentation.py
+++ tissueSegmentation.py
@@ -1,6 +1,6 @@
 import cv2
 import sys
-from os import listdir
+from os import listdir,path
 import numpy as np
 from PIL import Image
 import pytesseract
@@ -8,7 +8,8 @@ from pytesseract import image_to_string
 from matplotlib import pyplot as plt
 
 
-path = 'C:\Users\NISHANT\Desktop\Sem 3\RE\Code\Images'
+#path = 'C:\Users\NISHANT\Desktop\Sem 3\RE\Code\Images'
+path = os.path.join(os.path.dirname(__file__),os.path.join('..','Images'))
 images = [ f for f in listdir(path) ]
 img_no = 1
 
@@ -16,7 +17,7 @@ for image in images:
     img = cv2.imread(path+'\\'+image)
     cv2.imshow('Image', img)
     cv2.waitKey(0)
-    
+
     hs_img = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
     #split the image into HSV planes
     h, l, s = cv2.split(hs_img)
@@ -31,16 +32,16 @@ for image in images:
     h1 = cv2.cvtColor(cv2.merge([h,s1,v1]), cv2.COLOR_HSV2RGB)
     cv2.imshow('Hue Channels', h1)
     cv2.waitKey(0)
-   
+
     mser = cv2.MSER_create()
     regions = mser.detectRegions(hs_image)
     hulls = [cv2.convexHull(p.reshape(-1, 1, 2)) for p in regions[0]]
-    cv2.polylines(im3, hulls, 3, (0,255,0)) 
+    cv2.polylines(im3, hulls, 3, (0,255,0))
     cv2.imshow('norm',im3)
     key = cv2.waitKey(0)
     """
-    
-    
+
+
     lower = np.array([0,0, 0])
     upper = np.array([225,225, 225])
     mask = cv2.inRange(hs_img,lower, upper)
@@ -48,4 +49,4 @@ for image in images:
     result = cv2.bitwise_and(img,img,mask = mask)
     cv2.imwrite('output\Result'+str(img_no)+'.png',result)
     img_no = img_no+1
-    
+
